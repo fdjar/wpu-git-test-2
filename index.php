dd0842c4d84608193b40c8ae072294b225b00e67
fdjar <fadjarjanuar98@gmail.com>
2021-12-31 17:58:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti rem possimus explicabo voluptatem tempora dolores, iste fugit ratione odit hic. Libero sequi beatae sapiente esse hic tempora, placeat excepturi architecto.</p>
+    <ul>
+        <li>fadjar</li>
+    </ul>
 </body>
 
 </html>
